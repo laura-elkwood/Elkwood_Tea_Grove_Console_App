cce5a1b6a95cb1d34ec658bf9ce889ceed395d96
laura-elkwood <laura.elkwood@gmail.com>
2025-09-09 22:03:22 +0100

    And so it begins...

---
+++ ElkwoodTeaGroveConsoleApp/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("########## Welcome to Elkwood Tea Grove! ##########");
+        System.out.println("##########        1. View Teas           ##########");
+        System.out.println("##########        2. Add New Tea         ##########");
+        System.out.println("##########        3. Remove Tea          ##########");
+        System.out.println("##########        4. Edit Tea            ##########");
+        System.out.println("###################################################");
+
+        System.out.print("Please select menu option... ");
+        System.in.read();
+    }
+}
